0dde92e61d51defc4aba811ced396b55acce84e2
Francesco Scandiffio <francesco.scandiffio@stud.unifi.it>
2020-06-12 20:13:17 +0200

    Parallasse

--- ourFunctions.h
+++ ourFunctions.h
@@ -15,9 +15,16 @@ int changeCoordinates();
 
 int changeCoordinates2();
 
+int changeCoordinates3();
+
+void disegnaParallasse();
+
 cv::Point2f rotate_and_translate(cv::Point2f point);
 
 int drawPoints();
+
+std::pair<double, double> lineLineIntersection(std::pair<double, double> A, std::pair<double, double> B,
+                                               std::pair<double, double> C, std::pair<double, double> D);
 
 
 #endif //FOUCAULT_OURFUNCTIONS_H
