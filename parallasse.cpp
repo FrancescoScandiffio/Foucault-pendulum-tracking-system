7be0ada5c86b6cdf99a579db9142ad8c87f2b8f7
Francesco Scandiffio <francesco.scandiffio@stud.unifi.it>
2020-06-25 18:51:13 +0200

    Update parallasse.cpp

--- parallasse.cpp
+++ parallasse.cpp
@@ -28,7 +28,7 @@ int position_x=-1;
 int position_y=-1;
 
 void MatchingMethod( int, void* );
-Mat coord_image = Mat::zeros( 7000, 1200, CV_8UC3);
+Mat coord_image = Mat::zeros( 480, 640, CV_8UC3);
 
 void draw() {
     while (true) {
@@ -39,10 +39,14 @@ void draw() {
 
 
 /** @function main */
-void disegnaParallasse() {
+void disegnaParallasse(int val) {
 
-    changeCoordinates3();
-    ifstream input_txt ("../output3.txt");
+    if(val==0)
+        changeCoordinatesGeometry();
+    else
+        changeCoordinatesMatrix();
+
+    ifstream input_txt ("../giugno.txt");
     if (input_txt.is_open())
         cout << "Opened input file.txt\n";
 
@@ -56,7 +60,7 @@ void disegnaParallasse() {
     std::thread first(draw);
     first.detach();
 
-    while(getline(input_txt, line) && iter < 50000) {
+    while(getline(input_txt, line)) {
         // extract the coordinates
         int lBracket = line.find("(");
         int rBracket = line.find(")");
@@ -68,11 +72,11 @@ void disegnaParallasse() {
         y.erase(remove_if(y.begin(), y.end(), ::isspace),y.end());
 
 
-       new_pt = Point(stod(x), stod(y));
+       new_pt = Point(stod(x), 480-stod(y));
 
         // printing a line between the two points
         cv::line(coord_image, new_pt, new_pt, cv::Scalar(0,0,0), 2);
-        unsigned int t = 50;
+        unsigned int t = 1;
         usleep(t);
         iter++;
     }
@@ -131,29 +135,17 @@ Triple getPlaneLineIntersection(Plane plane, vectorLine line){
     return {point.x + vector.x*t,point.y + vector.y*t, point.z + vector.z*t };
 }
 
-int changeCoordinates3(){
+int changeCoordinatesGeometry(){
     //point: x,y,z where z is real-life height. The origin of the reference system is the bottom-left corner of the black
     //plate on the floor
-    double pixelCm = 1; //pixel length in cm
-    Triple cameraPoint = {25.3, 35, 176.5}; // real-life camera position (cm)
+    double pixelCm = 0.1; //1 pixel = 0.09774193548 cm
+    Triple cameraPoint = {25.3, 32, 176.5}; // real-life camera position (cm)
     Triple pendulumPoint = {30.6,32, 14.2};// real-life pendulum position (cm)
     vectorLine focusLine = makeLine(cameraPoint,pendulumPoint);
     Plane focusPlane = getPerpendicularPlane(focusLine, pendulumPoint);
 
 
-    //TEST
-    Triple coordinate1 = {256.08, 33.68, 0.0};
-    vectorLine test1 = makeLine(cameraPoint,coordinate1);
-    Triple intersectionTest = getPlaneLineIntersection(focusPlane, test1);
-
-    std::cout<<intersectionTest.x<<" "<<intersectionTest.y<<" "<<intersectionTest.z<<" "<<endl;
-
-
-    // frame height of raspberry
-    double height=480.0;
-
-
-    ifstream input_txt ("../Fri May 29 14-02-27 2020.txt");
+    ifstream input_txt ("../giugno.txt");
     if (input_txt.is_open())
         cout << "Opened input file.txt\n";
 
@@ -173,19 +165,61 @@ int changeCoordinates3(){
         string y = line.substr(comma+1, rBracket-comma-1);
         x.erase(remove_if(x.begin(), x.end(), ::isspace),x.end());
         y.erase(remove_if(y.begin(), y.end(), ::isspace),y.end());
-       // cout<<"x: "<<x<<"y: "<<y<<endl;
 
+        double oldX = stoi(x);
+        double oldY = stoi(y);
 
-        //cm coordinates
-        double oldX = stoi(x) * pixelCm;
-        double oldY = (height - stoi(y)) * pixelCm ;
-        Triple coordinate = {oldX, oldY, 0.0};
+        double cmX = oldX * pixelCm;
+        double cmY = oldY * pixelCm ;
+        Triple coordinate = {cmX, cmY, 14.2};
         vectorLine pointLine = makeLine(cameraPoint,coordinate);
         Triple intersection = getPlaneLineIntersection(focusPlane, pointLine);
 
-
         output_txt <<line.substr(0,lBracket)<<"("<<intersection.x/pixelCm<<","<<intersection.y/pixelCm<<")\n";
         output_txt.flush();
+
+    }
+
+    // closing the txt files
+    input_txt.close();
+    output_txt.close();
+    return 0;
+}
+
+
+
+int changeCoordinatesMatrix(){
+
+    ifstream input_txt ("../giugno.txt");
+    if (input_txt.is_open())
+        cout << "Opened input file.txt\n";
+
+    ofstream output_txt ("../output3.txt");
+    if (output_txt.is_open())
+        cout << "Opened output file.txt\n";
+
+    string line;
+    while(getline(input_txt, line)) {
+
+        int lBracket = line.find("(");
+        int rBracket = line.find(")");
+        int comma = line.find(",");
+
+        string x = line.substr(lBracket+1, comma-lBracket-1);
+        string y = line.substr(comma+1, rBracket-comma-1);
+        x.erase(remove_if(x.begin(), x.end(), ::isspace),x.end());
+        y.erase(remove_if(y.begin(), y.end(), ::isspace),y.end());
+
+        double oldX = stoi(x);
+        double oldY = stoi(y);
+
+        Point2d old = Point2d(oldX,oldY);
+        Point2d nuovo = singlePointPerspective(old);
+
+
+        output_txt <<line.substr(0,lBracket)<<"("<<nuovo.x<<","<<nuovo.y<<")\n";
+        output_txt.flush();
+
     }
 
     // closing the txt files
