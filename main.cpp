dc9581a1893d6a3e7524e9951d493eb9b42b0995
ClaudiaRaffaelli <claudia.raffaelli96@gmail.com>
2020-06-30 20:21:17 +0200

    Merged multithreading and perspective correction

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ using namespace cv;
 /// Global Variables
 Mat frame;
 Mat templ;
+double myMatrix[3][3];
 
 String image_window = "Source Image";
 Mat result_A;
@@ -33,6 +34,7 @@ std::queue<tuple<Mat, int, double, double, double>> resultQueue_B;
 /// Function Headers
 tuple<double, double> MatchingMethod( int, void*, const string&, Mat croppedFrame);
 void frameComputation(const string& whichThread);
+void writeFile();
 
 
 /** @function main */
@@ -47,59 +49,48 @@ int main() {
     // open the default camera
     //VideoCapture capture(0);
     // setting fps rate of video to grab
-    //capture.set(CAP_PROP_FPS, int(30));
+    //capture.set(CAP_PROP_FPS, int(9));
 
     ///If working locally:
     VideoCapture capture("../videos/output.mp4");
 
-    /// Create windows
-    namedWindow( image_window );
-    //namedWindow( result_window );
+    Mat originalFrame;
+    capture.read(originalFrame);
+    Point p1 = Point(32, 0);
+    Point p2 = Point(610, 0);
+    Point p3 = Point(23, 478);
+    Point p4 = Point(600, 475);
+    Point p5 = Point(0, 0);
 
-    /// Create Trackbar
-    String trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
-    //createTrackbar( trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod );
+    int frameWidth = originalFrame.size().width;
+    int frameHeight = originalFrame.size().height;
+    Point2f v1[] = {p1,p2,p3,p4};
+    Point p6 = Point(frameWidth, 0);
+    Point p7 = Point(0, frameHeight);
+    Point p8 = Point(frameWidth, frameHeight);
+    Point2f v2[] = {p5,p6,p7,p8};
+    Mat matrix = getPerspectiveTransform(v1, v2);
+    for(int i=0; i<3; i++)
+        for(int j=0; j<3; j++){
+            myMatrix[i][j] = matrix.at<double>(i,j);
+            cout<<myMatrix[i][j]<<"  ";
+        }
 
-    /// Getting the current time
-    chrono::system_clock::time_point p = chrono::system_clock::now();
-    time_t t = chrono::system_clock::to_time_t(p);
-    
     auto t0 = Time::now();
     auto t1 = Time::now();
     elapsedDouble ourElapsed=t1-t0;
     double start = false;
 
-    /// Setting the right name for the file that will store the centers positions
-    std::ostringstream oss;
-    oss << "../" << ctime(&t) << ".txt";
-    std::string file_name = oss.str();
-
-    /// Opening the file where will be saved the coordinates of centers on each frame
-    ofstream txt_file (file_name);
-    if (txt_file.is_open())
-        cout << "Opened file "<< file_name<<"\n";
-
     std::tuple<Mat, int, double> frameInfo;
-
     int frame_number=0;
-    int expectedFrameNumber=0;
-    bool alreadyExtracted_A = false;
-    bool alreadyExtracted_B = false;
-
-    int frameNumber_A=-1;
-    int frameNumber_B=-1;
-    double elapsed_A=-1.0;
-    double elapsed_B=-1.0;
-    double pos_X_A;
-    double pos_X_B;
-    double pos_Y_A;
-    double pos_Y_B;
-    Mat extracted_Mat_A;
-    Mat extracted_Mat_B;
 
     // starting the two threads that handle the frame computation here
     std::thread thread1 (frameComputation, "threadA");
     std::thread thread2 (frameComputation, "threadB");
+    std::thread thread3 (writeFile);
+    thread1.detach();
+    thread2.detach();
+    thread3.detach();
 
     while(true) {
 
@@ -128,58 +119,11 @@ int main() {
             frameQueue_B.push(frameInfo);
         }
 
-        // extracting elements from the first queue if not already done in the previous iteration without writing the
-        // result to the txt file
-        if(!alreadyExtracted_A){
-            if(!resultQueue_A.empty()){
-                tie(extracted_Mat_A, frameNumber_A, elapsed_A, pos_X_A, pos_Y_A) = resultQueue_A.front();
-                resultQueue_A.pop();
-                // we extracted the element from the queue
-                alreadyExtracted_A=true;
-            }
-        }
-        // if the frame extracted from resultQueue_A is the frame that has to be written in the txt (and we know it by
-        // looking at the current frame number expected to be written)
-        if(expectedFrameNumber==frameNumber_A){
-            // saving to txt the positions found in MatchingMethod
-            txt_file <<"time: "<< elapsed_A <<" position ("<< pos_X_A<<", "<<pos_Y_A<<")\n";
-            txt_file.flush();
-
-            // incrementing the expectedFrameNumber because we handled the frame and we can pass to the later one
-            expectedFrameNumber++;
-            alreadyExtracted_A=false;
-            imshow( image_window, extracted_Mat_A);
-            // show live and wait for a key with timeout long enough to show images
-            waitKey(1);
-        }
-        // doing the same for the other queue and thread
-        if(!alreadyExtracted_B){
-            if(!resultQueue_B.empty()){
-                tie(extracted_Mat_B, frameNumber_B, elapsed_B, pos_X_B, pos_Y_B) = resultQueue_B.front();
-                resultQueue_B.pop();
-                // we extracted the element from the queue
-                alreadyExtracted_B=true;
-            }
-        }
-        if(expectedFrameNumber==frameNumber_B){
-            // saving to txt the positions found in MatchingMethod
-            txt_file <<"time: "<< elapsed_B <<" position ("<< pos_X_B<<", "<<pos_Y_B<<")\n";
-            txt_file.flush();
-
-            // incrementing the expectedFrameNumber because we handled the frame and we can pass to the later one
-            expectedFrameNumber++;
-            alreadyExtracted_B=false;
-            imshow( image_window, extracted_Mat_B);
-            // show live and wait for a key with timeout long enough to show images
-            waitKey(1);
-        }
-
         frame_number++;
+        if (frame_number % 100 ==0)
+            cout<<frame_number<<" Elements in FrameA: "<<frameQueue_A.size()<<", elements in FrameB: "<<frameQueue_B.size()<<endl;
 
     }
-    txt_file <<ctime(&t)<<endl;;
-    // closing the txt file
-    txt_file.close();
 
     // the camera will be de-initialized automatically in VideoCapture destructor
     return 0;
@@ -230,7 +174,6 @@ tuple<double, double> MatchingMethod( int, void*, const string& whichThread, Mat
 
 void frameComputation(const string& whichThread){
     Mat myFrame;
-    Mat myCroppedFrame;
     int myFrameNumber;
     double ourElapsed;
     std::queue<std::tuple<Mat, int, double>> *frameQueue_X;
@@ -260,27 +203,118 @@ void frameComputation(const string& whichThread){
                 break;
             }
 
-            // getting frame size
-            Size s = myFrame.size();
-            int rows = s.height;
-            int cols = s.width;
-
-            // Cropping the frame to exclude unwanted area on video
-            // The area of interest is of the form Rect(Point(x, y), Point(x,y)) in which the first point indicates the
-            // top left corner of the box
-            myFrame(Rect(Point(30, 0), Point(cols-30,rows))).copyTo(myCroppedFrame);
-
             // we pass in MatchingMethod a view of the Mat myCroppedFrame. When the functions returns the content of
             // myCroppedFrame will be changed as we want (in order to show the tracking rectangle)
-            tuple<double, double> myResult = MatchingMethod(0, 0, whichThread, myCroppedFrame);
+            tuple<double, double> myResult = MatchingMethod(0, 0, whichThread, myFrame);
             tie( position_X, position_Y) = myResult;
 
-            // updating with proper value. At the moment y indicates the distance from the point to the top of the image
-            // now y is the distance from point to image bottom
-            double new_position_y = height_frame - position_Y;
+
+            double new_position_x = -1;
+            double new_position_y = -1;
+
+            double num = myMatrix[0][0]*position_X+myMatrix[0][1]*position_Y+myMatrix[0][2];
+            double dem = myMatrix[2][0]*position_X+myMatrix[2][1]*position_Y+myMatrix[2][2];
+
+            new_position_x = num/dem;
+
+            num = myMatrix[1][0]*position_X+myMatrix[1][1]*position_Y+myMatrix[1][2];
+            dem = myMatrix[2][0]*position_X+myMatrix[2][1]*position_Y+myMatrix[2][2];
+            new_position_y = height_frame - num/dem;
 
             // creating the output tuple of the form (cropped_frame, frame_number, time, position_x, position_y)
-            resultQueue_X->push(std::make_tuple(myCroppedFrame.clone(), myFrameNumber, ourElapsed, position_X, new_position_y));
+            resultQueue_X->push(std::make_tuple(myFrame.clone(), myFrameNumber, ourElapsed, new_position_x, new_position_y));
         }
     }
 }
+
+void writeFile(){
+
+    /// Getting the current time
+    chrono::system_clock::time_point p = chrono::system_clock::now();
+    time_t t = chrono::system_clock::to_time_t(p);
+
+    /// Setting the right name for the file that will store the centers positions
+    std::ostringstream oss;
+    oss << "../PendulumCsv/" << ctime(&t) << ".txt";
+    std::string file_name = oss.str();
+
+    /// Opening the file where will be saved the coordinates of centers on each frame
+    ofstream txt_file (file_name);
+    if (txt_file.is_open())
+        cout << "Opened file "<< file_name<<"\n";
+
+    txt_file <<"time x y\n";
+
+    int expectedFrameNumber=0;
+    bool alreadyExtracted_A = false;
+    bool alreadyExtracted_B = false;
+
+    int frameNumber_A=-1;
+    int frameNumber_B=-1;
+    double elapsed_A=-1.0;
+    double elapsed_B=-1.0;
+    double pos_X_A;
+    double pos_X_B;
+    double pos_Y_A;
+    double pos_Y_B;
+
+    Mat extracted_Mat_A;
+    Mat extracted_Mat_B;
+
+    while(true){
+
+        this_thread::sleep_for(chrono::seconds(5));
+        for(int iter=0; iter<100; iter++){
+
+            // extracting elements from the first queue if not already done in the previous iteration without writing the
+            // result to the txt file
+            if(!alreadyExtracted_A){
+                if(!resultQueue_A.empty()){
+                    tie(extracted_Mat_A, frameNumber_A, elapsed_A, pos_X_A, pos_Y_A) = resultQueue_A.front();
+                    resultQueue_A.pop();
+                    // we extracted the element from the queue
+                    alreadyExtracted_A=true;
+                }
+            }
+            // if the frame extracted from resultQueue_A is the frame that has to be written in the txt (and we know it by
+            // looking at the current frame number expected to be written)
+            if(expectedFrameNumber==frameNumber_A){
+                // saving to txt the positions found in MatchingMethod
+                txt_file <<elapsed_A <<" "<<pos_X_A<<" "<<pos_Y_A<<"\n";
+                txt_file.flush();
+
+                // incrementing the expectedFrameNumber because we handled the frame and we can pass to the later one
+                expectedFrameNumber++;
+                alreadyExtracted_A=false;
+                imshow( image_window, extracted_Mat_A);
+                // show live and wait for a key with timeout long enough to show images
+                waitKey(1);
+            }
+            // doing the same for the other queue and thread
+            if(!alreadyExtracted_B){
+                if(!resultQueue_B.empty()){
+                    tie(extracted_Mat_B, frameNumber_B, elapsed_B, pos_X_B, pos_Y_B) = resultQueue_B.front();
+                    resultQueue_B.pop();
+                    // we extracted the element from the queue
+                    alreadyExtracted_B=true;
+                }
+            }
+            if(expectedFrameNumber==frameNumber_B){
+                // saving to txt the positions found in MatchingMethod
+                txt_file << elapsed_B<<" "<<pos_X_B<<" "<<pos_Y_B<<"\n";
+                txt_file.flush();
+
+                // incrementing the expectedFrameNumber because we handled the frame and we can pass to the later one
+                expectedFrameNumber++;
+                alreadyExtracted_B=false;
+                imshow( image_window, extracted_Mat_B);
+                // show live and wait for a key with timeout long enough to show images
+                waitKey(1);
+            }
+        }
+    }
+    txt_file <<ctime(&t)<<endl;;
+    // closing the txt file
+    txt_file.close();
+}
+
